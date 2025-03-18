79a679eaf511a2d0170a3948a2009e0bb64c3998
Mert Özbolat <122216794+Mert-Ozbolat@users.noreply.github.com>
2025-03-18 21:02:39 +0300

    update

--- src/app/components/navbar/nav-link.tsx
+++ src/app/components/navbar/nav-link.tsx
@@ -1,28 +1,30 @@
-'use client'
+"use client";
 
-import { Option } from '@/app/utils/constants'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import React, { FC } from 'react'
-
+import { Option } from "@/app/utils/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { FC } from "react";
 
 type Props = {
-    item: Option
-}
-
+    item: Option;
+};
 
 const NavLink: FC<Props> = ({ item }) => {
-
     const path = usePathname();
-    const isActive = path === item.url
 
+    const isActive = path === item.url;
 
     return (
-        <Link className={`flex gap-2 items-center p-5 hover:bg-gray-100 transition border-l-2 border-transparent ${isActive && "text-blue-500 !border-blue-500"}`} href={item?.url || '/'}>
+        <Link
+            href={item?.url || "/"}
+            className={`flex gap-2 items-center p-5 hover:bg-gray-100 transition border-l-2 border-transparent ${isActive && "text-blue-500 !border-blue-500"
+                }`}
+        >
             {item.icon}
-            <span className='nav-text'>{item.name}</span>
-        </Link>
-    )
-}
 
-export default NavLink
+            <span className="nav-text">{item.name}</span>
+        </Link>
+    );
+};
+
+export default NavLink;
